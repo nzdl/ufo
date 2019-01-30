6a8920b904888618734dcfa4140f51ba354bd7fc
namelos <namelos@outlook.com>
2019-01-30 23:26:57 +0800

    add heroku deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hi')
+  res.send('hi traivs & heroku')
 })
 
 const port = process.env.PORT || 3000
