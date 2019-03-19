184026b58e21096c423fd65939d447a170d536ed
namelos <namelos@outlook.com>
2019-03-19 21:29:57 +0800

    cleanup

--- src/client/components/BoardApp.tsx
+++ src/client/components/BoardApp.tsx
@@ -1,46 +1,36 @@
-import * as React from "react";
-import { observable, computed } from 'mobx'
-import { inject, observer } from 'mobx-react'
-import axios from 'axios'
-import {boardStore, List} from "../stores/BoardStore";
+import { observer } from 'mobx-react'
+import * as React from 'react'
+import { boardStore, Card, List } from '../stores/BoardStore'
 
-let boardStyle = {
-    display : "flex",
-};
-let listStyle = {
-    border: "1px solid black"
-};
-let addListStyle = {
-    height: "30px",
-    display: "flex",
-};
+const boardStyle = {
+  display: 'flex'
+}
 
+const listStyle = {
+  border: '1px solid black'
+}
 
-export let ListView = observer((list) => {
+export const ListView = observer((list: List) => {
+  return <div style={listStyle}>
+    <div>{list.cards.map((card) => <CardView {...card} />)}</div>
+    <div>{list.listName}</div>
 
-    return <div style={listStyle}>
-        <div>{list.cards.map((card) => <CardView {...card} />)}</div>
-        <div>{list.listName}</div>
-
-        <input onChange={e => list.changeTitleInput(e.target.value) } value={list.titleInput}/>
-        <button onClick={e => list.addCard(list.titleInput)}>Add Card</button>
-    </div>
+    <input onChange={e => list.changeTitleInput(e.target.value)} value={list.titleInput} />
+    <button onClick={() => list.addCard()}>Add Card</button>
+  </div>
 })
 
-export let BoardApp = observer (() => {
-     return <div style={boardStyle}>
-         {boardStore.lists.map(
-             (list: List) => <ListView {...list} />
-         )}
-         <input style={addListStyle}/>
-         <button style={addListStyle} onClick={boardStore.addList}>Add List</button>
-     </div>
+export const BoardApp = observer(() => {
+  return <div style={boardStyle}>
+    {boardStore.lists.map((list: List) => <ListView {...list} />)}
 
- })
+    <button onClick={boardStore.addList}>Add List</button>
+  </div>
+})
 
-export let CardView = observer((card) => {
-    return <div>
-        <span>{card.title}</span>
-        <button onClick={e => card.deleteCard()}>x</button>
-    </div>
+export const CardView = observer((card: Card) => {
+  return <div>
+    <span>{card.title}</span>
+    <button onClick={() => card.deleteCard()}>x</button>
+  </div>
 })
--- src/client/stores/BoardStore.ts
+++ src/client/stores/BoardStore.ts
@@ -1,83 +1,51 @@
-import {observable} from "mobx";
-import axios from "axios";
-//import {create} from "domain";
-
+import { observable } from "mobx"
 
 export class BoardStore {
+  @observable cards = []
+  @observable lists = []
 
-    @observable cards =  [];
-
-    @observable lists = [
-        new List(
-            "list1",
-            []
-            ),
-    ];
-
-    addList = () => {
-        let list = new List(
-            "list1",
-            [
-            ])
-        this.lists.push(list)
-    }
-
-
-    fetchLists = async () => {
-        const response = await axios.get('/lists')
-        this.lists = response.data
-
-    }
-
-    fetchCards = async () => {
-        const response = await axios.get('/lists/:id')
-        this.lists = response.data
-
-    }
-
-}
-
-async function create({ list }) {
-    return await axios.post('/lists', { list })
+  addList = () => {
+    let list = new List("list1", [])
+    this.lists.push(list)
+  }
 }
 
 export class List {
+  @observable cards: Card[]
+  @observable listName: string
+  @observable titleInput = ""
 
-    @observable cards: Card[];
-    @observable listName : string;
-    @observable titleInput = "";
-
-  constructor(listName, cards){
+  constructor(listName, cards) {
     this.cards = cards
   }
 
   changeTitleInput = (title) => {
-      this.titleInput = title
-  };
+    this.titleInput = title
+  }
 
   addCard = () => {
-      let card = new Card("card789", this.titleInput, this)
-      this.cards.push(card)
-      this.titleInput = "";
-    };
+    let card = new Card("card789", this.titleInput, this)
+    this.cards.push(card)
+    this.titleInput = ""
+  };
 
   listDeleteCard = (card) => {
-      this.cards =  this.cards.filter(c => c !== card)
+    this.cards = this.cards.filter(c => c !== card)
   }
 }
 
 export class Card {
-    @observable cardName:string;
-    @observable title: string;
-    @observable list: List;
+  @observable cardName: string;
+  @observable title: string;
+  @observable list: List;
 
-  constructor(cardName, title, list){
+  constructor(cardName, title, list) {
     this.title = title
-      this.list = list
+    this.list = list
   }
 
   deleteCard = () => {
-      this.list.listDeleteCard(this)
+    this.list.listDeleteCard(this)
   }
 }
 
