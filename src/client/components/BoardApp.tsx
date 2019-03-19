ce39839b31d7ddca29bd85833ca442f5bf668b0b
namelos <namelos@outlook.com>
2019-03-19 21:36:00 +0800

    add simple styles

--- src/client/components/BoardApp.tsx
+++ src/client/components/BoardApp.tsx
@@ -2,14 +2,6 @@ import { observer } from 'mobx-react'
 import * as React from 'react'
 import { boardStore, Card, List } from '../stores/BoardStore'
 
-const boardStyle = {
-  display: 'flex'
-}
-
-const listStyle = {
-  border: '1px solid black'
-}
-
 export const ListView = observer((list: List) => {
   return <div style={listStyle}>
     <div>{list.cards.map((card) => <CardView {...card} />)}</div>
@@ -24,13 +16,32 @@ export const BoardApp = observer(() => {
   return <div style={boardStyle}>
     {boardStore.lists.map((list: List) => <ListView {...list} />)}
 
-    <button onClick={boardStore.addList}>Add List</button>
+    <div style={listStyle}>
+      <button onClick={boardStore.addList}>Add List</button>
+    </div>
   </div>
 })
 
+
 export const CardView = observer((card: Card) => {
-  return <div>
+  return <div style={cardStyle}>
     <span>{card.title}</span>
     <button onClick={() => card.deleteCard()}>x</button>
   </div>
 })
+
+const boardStyle = {
+  padding: 10,
+  display: 'flex',
+  border: '1px solid lightgray'
+}
+
+const listStyle = {
+  margin: 10,
+  padding: 14,
+  border: '1px solid lightgray'
+}
+
+const cardStyle = {
+  margin: '10px 0',
+}
